d206b76d11a45705ef7018f61579a5494afb1a67
FerleyPardo <boxbonny1234@gmail.com>
2023-11-19 14:23:29 -0500

    llavemala

--- config/conexion.php
+++ config/conexion.php
@@ -1,31 +1,32 @@
 <?php
-    /*TODO: Inicializando la sesion del usuario */
-    session_start();
+/* TODO: Inicializando la sesion del usuario */
+session_start();
 
-    /*TODO: Iniciamos Clase Conectar */
-    class Conectar{
-        protected $dbh;
+/* TODO: Iniciamos Clase Conectar */
+class Conectar
+{
+    protected $dbh;
 
-        /*TODO: Funcion Protegida de la cadena de Conexion */
-        protected function Conexion(){
-            try {
-        		$conectar = $this->dbh = new PDO("mysql:host=127.0.0.1; dbname=ciclo_rar
-                ","root","");
-        		return $conectar;
-			} catch (Exception $e) {
-        		print "¡Error BD!: " . $e->getMessage() . "<br/>";
-				die();
-			}
-        
-
-        public function set_names(){
-            return $this->dbh->query("SET NAMES 'utf8'");
+    /* TODO: Funcion Protegida de la cadena de Conexion */
+    protected function Conexion()
+    {
+        try {
+            $conectar = $this->dbh = new PDO("mysql:host=127.0.0.1; dbname=ciclo_rar", "root", "");
+            return $conectar;
+        } catch (Exception $e) {
+            print "¡Error BD!: " . $e->getMessage() . "<br/>";
+            die();
         }
-
-        public static function ruta(){
-            //QA
-            return "http://localhost/venta_p/";
-        }
-
     }
+
+    public function set_names()
+    {
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+
+    public static function ruta()
+    {
+        return "http://localhost/Venta_p/";
+    }
+}
 ?>
